78ca55c9b2a6fdda550ce2e4893ad0aae4ddeb3c
Stewie-pixel <hanchivi39@gmail.com>
2026-03-19 00:24:54 +1100

    feat: Add Snippet5

---
+++ task/Snippet5.cs
@@ -0,0 +1,10 @@
+using System;
+class Snippet5 {
+    static void Main() {
+        int z = 5;
+        while (z > 0) {
+            z = z - 1;
+            Console.WriteLine("z: " + z);
+        }
+    }
+}
